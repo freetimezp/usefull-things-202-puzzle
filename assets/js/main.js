55964232768c22daeb2b8bab041b2da8b7939900
Yevhen Onoprienko <fromzpcity@gmail.com>
2025-07-08 23:28:08 +0300

    04. usefull-things-202-puzzle. Drag, drop.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let parent = document.getElementById("drag");
     let frag = document.createDocumentFragment();
 
-    let elements = document.querySelectorAll(".dragBox");
+    let elements = document.querySelectorAll(".drag .dragBox");
 
     for (let i = 0; i < elements.length; i++) {
         frag.appendChild(parent.children[Math.floor(Math.random() * parent.children.length)]);
@@ -10,3 +10,18 @@ document.addEventListener("DOMContentLoaded", () => {
         parent.appendChild(frag);
     }
 });
+
+function drag(event) {
+    event.dataTransfer.setData("text", event.target.id);
+}
+
+function allowDrop(event) {
+    event.preventDefault();
+}
+
+function drop(event) {
+    event.preventDefault();
+
+    let data = event.dataTransfer.getData("text");
+    event.target.appendChild(document.getElementById(data));
+}
